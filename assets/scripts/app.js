6c9904599ed2a8ee4f08e565f0a10c29e88b1c6e
juliojesse25 <juliojesse25@gmail.com>
2019-02-20 11:45:10 -0500

    Modified app.js - Changed the gameBoard - Added a clickBoard function for what happens when the gameBoard is clicked. - Added a gamePlay function that calls the clickBoard function. - Added a win function in the clickBoard function that checks if a player has won the game. _ Added an html alert for when a player makes an invalid move. - Added a newGame function that resets the game. - Called the newGame function when the restart button is clicked.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,100 +2,100 @@
 
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
-// const gameEvents = require('./games/events')
+const authEvents = require('./games/events')
 // const getFormFields = require('../../lib/get-form-fields')
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
-// const onSignUp = () => {
-// return $.ajax({
-//   url:
-//   method: 'POST'
-// })
-// }
-
 $(() => {
   // your JS code goes here
-  let currentPlayer = 'X'
+  const playerOne = 'X'
+  const playerTwo = 'O'
 
-  const gameWinner = () => {
-    if (gameBoard[0] === gameBoard[1] === gameBoard[2] || gameBoard[0] === gameBoard[3] === gameBoard[6] || gameBoard[0] === gameBoard[4] === gameBoard[8] || gameBoard[3] === gameBoard[4] === gameBoard[5] || gameBoard[6] === gameBoard[7] === gameBoard[8] || gameBoard[2] === gameBoard[4] === gameBoard[6] || gameBoard[2] === gameBoard[5] === gameBoard[8]) {
-      console.log('The Winner is: ' + currentPlayer)
-    }
+  const possibleWins = [
+    ['cell1', 'cell2', 'cell3'],
+    ['cell4', 'cell5', 'cell6'],
+    ['cell7', 'cell8', 'cell9'],
+    ['cell1', 'cell4', 'cell7'],
+    ['cell2', 'cell5', 'cell8'],
+    ['cell3', 'cell6', 'cell9'],
+    ['cell1', 'cell5', 'cell9'],
+    ['cell3', 'cell5', 'cell7']
+  ]
+
+  const gameWin = (superset, subset) => {
+    return subset.every(function (value) {
+      return (superset.indexOf(value) >= 0)
+    })
   }
 
+  let playerOneMoves = []
+  let playerTwoMoves = []
+
+  let currentPlayer = playerOne
+  let currentMove = playerOneMoves
+
+  // $('#gameInfo').text(`It is ${currentPlayer}'s turn`)
   const switchPlayer = () => {
-    if (currentPlayer === 'O') {
-      currentPlayer = 'X'
+    if (currentPlayer === playerOne) {
+      currentPlayer = playerTwo
+      currentMove = playerTwoMoves
     } else {
-      currentPlayer = 'O'
+      currentPlayer = playerOne
+      currentMove = playerOneMoves
     }
     return currentPlayer
   }
 
-  let gameBoard = ['', '', '', '', '', '', '', '', '']
+  const clickBoard = function (event) {
+    console.dir(event.target.innerText)
+    if (!event.target.innerText) {
+      event.target.innerText = currentPlayer
 
-  $('#cell1').on('click', (event) => {
-    $(event.target).text(switchPlayer)
-    gameBoard[0] = currentPlayer
-    gameWinner()
-    console.log(gameBoard)
-  })
+      console.log(currentPlayer)
+      currentMove.push(event.target.id)
+      if (playerOneMoves.length + playerTwoMoves.length === 9) {
+        $('#gameInfo').text('It is a draw!')
+      }
+      possibleWins.forEach(possibleWin => {
+        const win = gameWin(currentMove, possibleWin)
+        if (win) {
+          $('#gameBoard').off('click', clickBoard)
+          $('#gameInfo').text('The Winner is ' + currentPlayer)
+        }
+      })
 
-  $('#cell2').on('click', (event) => {
-    $(event.target).text(switchPlayer)
-    gameBoard[1] = currentPlayer
-    gameWinner()
-  })
+      switchPlayer()
+      $('#gameInfo').text(`It is ${currentPlayer}'s turn`)
+    } else {
+      $('#player-feedback').text('Invalid move')
+      setTimeout(() => {
+        $('#player-feedback').text('')
+        $('#player-feedback').removeClass('error')
+      }, 1000)
+    }
+  }
 
-  $('#cell3').on('click', (event) => {
-    $(event.target).text(switchPlayer)
-    gameBoard[2] = currentPlayer
-    gameWinner()
-  })
+  const gamePlay = () => {
+    $('#gameBoard').on('click', clickBoard)
+  }
+  gamePlay()
 
-  $('#cell4').on('click', (event) => {
-    $(event.target).text(switchPlayer)
-    gameBoard[3] = currentPlayer
-    gameWinner()
-  })
-
-  $('#cell5').on('click', (event) => {
-    $(event.target).text(switchPlayer)
-    gameBoard[4] = currentPlayer
-    gameWinner()
-  })
-
-  $('#cell6').on('click', (event) => {
-    $(event.target).text(switchPlayer)
-    gameBoard[5] = currentPlayer
-    gameWinner()
-  })
-
-  $('#cell7').on('click', (event) => {
-    $(event.target).text(switchPlayer)
-    gameBoard[6] = currentPlayer
-    gameWinner()
-  })
-
-  $('#cell8').on('click', (event) => {
-    $(event.target).text(switchPlayer)
-    gameBoard[7] = currentPlayer
-    gameWinner()
-  })
-
-  $('#cell9').on('click', (event) => {
-    $(event.target).text(switchPlayer)
-    gameBoard[8] = currentPlayer
-    gameWinner()
-    console.log(gameBoard)
-  })
-
-  $('#restart').on('click', function (event) {
+  const newGame = function (event) {
     event.preventDefault()
     $('.cells').text('')
-    gameBoard = ['', '', '', '', '', '', '', '', '']
-    console.log('Game Board: ' + gameBoard)
-  })
+    playerOneMoves = []
+    playerTwoMoves = []
+    $('#gameBoard').on('click', clickBoard)
+    currentMove = playerOneMoves
+    switchPlayer()
+  }
+
+  $('#restart').on('click', newGame)
+
+  $('#sign-up-form').on('submit', authEvents.onSignUp)
+  $('#sign-in-form').on('submit', authEvents.onSignIn)
+  $('#change-password-form').on('submit', authEvents.onChangePassword)
+  $('#sign-out-button').on('click', authEvents.onSignOut)
 })
