178901ccec0cabc07514b942d294e9ac95555f88
juliojesse25 <juliojesse25@gmail.com>
2019-02-20 22:31:41 -0500

    Modified ui.js
    -Added missing user messages to the api success and failure events.

--- assets/scripts/games/ui.js
+++ assets/scripts/games/ui.js
@@ -1,7 +1,14 @@
 'use strict'
 const store = require('../store.js')
-const signUpSuccess = function () {
-
+const signUpSuccess = () => {
+  $('#gameInfo').text('Sign Up Successful!')
+  $('#gameInfo').css('color', 'green')
+  setTimeout(() => $('#gameInfo').text(''), 2000)
+}
+const signUpFailure = () => {
+  $('#gameInfo').text('Error on Sign Up!')
+  $('#gameInfo').css('red')
+  setTimeout(() => $('#gameInfo').text(''), 2000)
 }
 
 const signInSuccess = function (responseData) {
@@ -12,31 +19,54 @@ const signInSuccess = function (responseData) {
   $('#change-password-dropDown').show()
   $('#sign-up-dropDown').hide()
   $('#gameInfo').text(`Successfully signed in!`)
+  $('#gameInfo').css('color', 'green')
   setTimeout(() => $('#gameInfo').text(`It is X's turn`), 2000)
   store.user = responseData.user
   console.log(responseData.user)
 }
 const signInFailure = () => {
-  $('#user-message').text('Email and Password do not match')
-  $('#user-message').css('color', 'red')
+  $('#gameInfo').text('Email and Password do not match')
+  $('#gameInfo').css('color', 'red')
+  setTimeout(() => $('#gameInfo').text(''), 2000)
 }
 const changePasswordSuccess = function () {
   $('#gameInfo').text('Successfully Changed Password!')
+  $('#gameInfo').css('color', 'green')
+  setTimeout(() => $('#gameInfo').text(''), 2000)
 }
+
+const changePasswordFailure = () => {
+  $('#gameInfo').text('Error in changing Password')
+  $('#gameInfo').css('color', 'red')
+  setTimeout(() => $('#gameInfo').text(''), 2000)
+}
+
 const signOutSuccess = function () {
   $('#gameInfo').text('Successfully Signed Out!')
   $('#gameBoard').hide()
   $('#authForm').show()
   $('#restart').hide()
+  $('#sign-out-button').hide()
+  $('#change-password-dropDown').hide()
+  $('#sign-up-dropDown').show()
   $('form').trigger('reset')
   store.user = null
   console.log('store:', store)
   $('#gameInfo').css('color', 'green')
+  setTimeout(() => $('#gameInfo').text(''), 2000)
+}
+const signOutFailure = () => {
+  $('#gameInfo').text('An Error Occured')
+  $('#gameInfo').css('color', 'red')
+  setTimeout(() => $('#gameInfo').text(''), 2000)
 }
 module.exports = {
   signUpSuccess,
+  signUpFailure,
   signInSuccess,
+  signInFailure,
   changePasswordSuccess,
+  changePasswordFailure,
   signOutSuccess,
-  signInFailure
+  signOutFailure
 }
